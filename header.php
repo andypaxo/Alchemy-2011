f10da36a479045029b2e0bd60699cb7a6738c86b
andypaxo <andy@andypaxo.net>
2010-11-14 16:35:52 -0700

    added html5 reset, so we can do some backwards compatible html5

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php wp_title(); bloginfo('name'); ?></title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(), '/html5reset.css'; ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
 </head>
