ee68d8776dc7f021aa5b820bb86d6a99605c9bc8
andypaxo <andy@andypaxo.net>
2010-11-14 17:46:47 -0700

    added placeholder navigation links

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php wp_title(); bloginfo('name'); ?></title>
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(), '/html5reset.css'; ?>" />
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -13,7 +14,14 @@
 <body <?php body_class()?>><div id='main'>
 
 <header>
-<h1>
-<a href='<?php bloginfo('url')?>' alt='<?php bloginfo('name'); ?>'><img src='<?php bloginfo('template_url'); ?>/title.png'/></a>
-</h1>
+	<h1>
+		<a href='<?php bloginfo('url')?>'><img alt='<?php bloginfo('name'); ?>' src='<?php bloginfo('template_url'); ?>/title.png'/></a>
+	</h1>
+	<nav>
+		<ul>
+			<li><a href='#'><img src='<?php bloginfo('template_url'); ?>/about.png' alt='about' /></a></li>
+			<li><a href='#'><img src='<?php bloginfo('template_url'); ?>/search.png' alt='search' /></a></li>
+			<li><a href='#'><img src='<?php bloginfo('template_url'); ?>/subscribe.png' alt='subscribe' /></a></li>
+		</ul>
+	</nav>
 </header>
