01d16c42870df139045cc4b0cbefd24345551cb2
andypaxo <andy@andypaxo.net>
2010-11-14 17:07:08 -0700

    added title image

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
+</div><!-- #main -->
+
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,10 +4,16 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php wp_title(); bloginfo('name'); ?></title>
-<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(), '/html5reset.css'; ?>" />
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class()?>><div id='main'>
+
+<header>
+<h1>
+<a href='<?php bloginfo('url')?>' alt='<?php bloginfo('name'); ?>'><img src='<?php bloginfo('template_url'); ?>/title.png'/></a>
+</h1>
+</header>
